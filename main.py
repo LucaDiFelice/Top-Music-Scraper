d71d40194a485c6052041ed5b5dc58ec02045046
Luca Di Felice <Luca.difelice@hts.on.ca>
2023-08-16 14:01:06 -0400

    the program works headless now :)

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 if __name__ == "__main__":
+    options = Options()
+    options.add_argument("--headless=new")
     url = "https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M"
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome(options=options)
     browser.get(url)
-    browser.set_window_size(1632, 1025)
     browser.execute_script("window.scrollTo(0,0)")
     song = browser.find_element(By.CSS_SELECTOR,
                                 "div:nth-child(1) > .h4HgbO_Uu1JYg5UGANeQ .t_yrXoUO3qGsJS4Y6iXX > .Type__TypeElement-sc-goli3j-0").get_property("innerHTML")
