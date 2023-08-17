212a0cb700364706f80e06060fe2fe8a841b32f6
Luca Di Felice <Luca.difelice@hts.on.ca>
2023-08-17 12:19:17 -0400

    it tells you 3 of the todays top hits songs

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
+
 if __name__ == "__main__":
     options = Options()
     options.add_argument("--headless=new")
+    options.add_argument("--window-size=2560,1440")
     url = "https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M"
     browser = webdriver.Chrome(options=options)
     browser.get(url)
-    browser.execute_script("window.scrollTo(0,0)")
+
+    time.sleep(2)
+
     song = browser.find_element(By.CSS_SELECTOR,
                                 "div:nth-child(1) > .h4HgbO_Uu1JYg5UGANeQ .t_yrXoUO3qGsJS4Y6iXX > .Type__TypeElement-sc-goli3j-0").get_property("innerHTML")
+
     print(song)
-    browser.execute_script("window.scrollTo(0,1)")
+
+    time.sleep(1)
+
     song2 = browser.find_element(By.CSS_SELECTOR,
                                 "div:nth-child(2) > .h4HgbO_Uu1JYg5UGANeQ .t_yrXoUO3qGsJS4Y6iXX > .Type__TypeElement-sc-goli3j-0").get_property("innerHTML")
     print(song2)
+
+    time.sleep(1)
+
+    song3 = browser.find_element(By.CSS_SELECTOR,
+                                "div:nth-child(3) > .h4HgbO_Uu1JYg5UGANeQ .t_yrXoUO3qGsJS4Y6iXX > .Type__TypeElement-sc-goli3j-0").get_property("innerHTML")
+    print(song3)
+
+    browser.quit()
 
