d77eae5e661eae434e9086413c38a41506c1f3d4
Luca Di Felice <Luca.difelice@hts.on.ca>
2023-08-17 15:50:00 -0400

    it tells you 10 of the todays top hits songs

--- main.py
+++ main.py
@@ -15,22 +15,73 @@ if __name__ == "__main__":
 
     time.sleep(2)
 
-    song = browser.find_element(By.CSS_SELECTOR,
-                                "div:nth-child(1) > .h4HgbO_Uu1JYg5UGANeQ .t_yrXoUO3qGsJS4Y6iXX > .Type__TypeElement-sc-goli3j-0").get_property("innerHTML")
-
-    print(song)
+    song1 = browser.find_element(By.CSS_SELECTOR,
+                                "div:nth-child(1) > .h4HgbO_Uu1JYg5UGANeQ .t_yrXoUO3qGsJS4Y6iXX > \
+                                .Type__TypeElement-sc-goli3j-0").get_property("innerHTML")
+    print(song1)
 
     time.sleep(1)
 
     song2 = browser.find_element(By.CSS_SELECTOR,
-                                "div:nth-child(2) > .h4HgbO_Uu1JYg5UGANeQ .t_yrXoUO3qGsJS4Y6iXX > .Type__TypeElement-sc-goli3j-0").get_property("innerHTML")
+                                "div:nth-child(2) > .h4HgbO_Uu1JYg5UGANeQ .t_yrXoUO3qGsJS4Y6iXX > \
+                                .Type__TypeElement-sc-goli3j-0").get_property("innerHTML")
     print(song2)
 
     time.sleep(1)
 
     song3 = browser.find_element(By.CSS_SELECTOR,
-                                "div:nth-child(3) > .h4HgbO_Uu1JYg5UGANeQ .t_yrXoUO3qGsJS4Y6iXX > .Type__TypeElement-sc-goli3j-0").get_property("innerHTML")
+                                "div:nth-child(3) > .h4HgbO_Uu1JYg5UGANeQ .t_yrXoUO3qGsJS4Y6iXX > \
+                                .Type__TypeElement-sc-goli3j-0").get_property("innerHTML")
     print(song3)
+
+    time.sleep(1)
+
+    song4 = browser.find_element(By.CSS_SELECTOR,
+                                "div:nth-child(4) > .h4HgbO_Uu1JYg5UGANeQ .t_yrXoUO3qGsJS4Y6iXX > \
+                                .Type__TypeElement-sc-goli3j-0").get_property("innerHTML")
+    print(song4)
+
+    time.sleep(1)
+
+    song5 = browser.find_element(By.CSS_SELECTOR,
+                                "div:nth-child(5) > .h4HgbO_Uu1JYg5UGANeQ .t_yrXoUO3qGsJS4Y6iXX > \
+                                .Type__TypeElement-sc-goli3j-0").get_property("innerHTML")
+    print(song5)
+
+    time.sleep(1)
+
+    song6 = browser.find_element(By.CSS_SELECTOR,
+                                "div:nth-child(6) > .h4HgbO_Uu1JYg5UGANeQ .t_yrXoUO3qGsJS4Y6iXX > \
+                                .Type__TypeElement-sc-goli3j-0").get_property("innerHTML")
+    print(song6)
+
+    time.sleep(1)
+
+    song7 = browser.find_element(By.CSS_SELECTOR,
+                                "div:nth-child(7) > .h4HgbO_Uu1JYg5UGANeQ .t_yrXoUO3qGsJS4Y6iXX > \
+                                .Type__TypeElement-sc-goli3j-0").get_property("innerHTML")
+    print(song7)
+
+    time.sleep(1)
+
+    song8 = browser.find_element(By.CSS_SELECTOR,
+                                "div:nth-child(8) > .h4HgbO_Uu1JYg5UGANeQ .t_yrXoUO3qGsJS4Y6iXX > \
+                                .Type__TypeElement-sc-goli3j-0").get_property("innerHTML")
+    print(song8)
+
+    time.sleep(1)
+
+    song9 = browser.find_element(By.CSS_SELECTOR,
+                                "div:nth-child(9) > .h4HgbO_Uu1JYg5UGANeQ .t_yrXoUO3qGsJS4Y6iXX > \
+                                .Type__TypeElement-sc-goli3j-0").get_property("innerHTML")
+    print(song9)
+
+    time.sleep(1)
+
+    song10 = browser.find_element(By.CSS_SELECTOR,
+                                "div:nth-child(10) > .h4HgbO_Uu1JYg5UGANeQ .t_yrXoUO3qGsJS4Y6iXX > \
+                                .Type__TypeElement-sc-goli3j-0").get_property("innerHTML")
+    print(song10)
 
     browser.quit()
 
