037a331309ac2248adc1eaf02ed392b067be1e8b
Luca Di Felice <Luca.difelice@hts.on.ca>
2023-08-21 19:35:56 -0400

    it can now scrape the top 32 songs, i want it to scrape 50 songs tho :(

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from Today_Top_Hits import Top_50
 
 
 if __name__ == "__main__":
@@ -15,16 +16,25 @@ if __name__ == "__main__":
 
     time.sleep(2)
 
+    for i in range(0, len(Top_50)):
+
+        Song = browser.find_element(By.CSS_SELECTOR, Top_50[i]).get_property("innerHTML")
+
+        print(Song)
+
+        time.sleep(1)
+
+    '''
     song1 = browser.find_element(By.CSS_SELECTOR,
-                                "div:nth-child(1) > .h4HgbO_Uu1JYg5UGANeQ .t_yrXoUO3qGsJS4Y6iXX > \
-                                .Type__TypeElement-sc-goli3j-0").get_property("innerHTML")
+                                 "div:nth-child(1) >.h4HgbO_Uu1JYg5UGANeQ .t_yrXoUO3qGsJS4Y6iXX > \
+                                 .Type__TypeElement-sc-goli3j-0", ).get_property("innerHTML")
     print(song1)
 
     time.sleep(1)
 
     song2 = browser.find_element(By.CSS_SELECTOR,
-                                "div:nth-child(2) > .h4HgbO_Uu1JYg5UGANeQ .t_yrXoUO3qGsJS4Y6iXX > \
-                                .Type__TypeElement-sc-goli3j-0").get_property("innerHTML")
+                                 "div:nth-child(2) >.h4HgbO_Uu1JYg5UGANeQ .t_yrXoUO3qGsJS4Y6iXX > \
+                                 .Type__TypeElement-sc-goli3j-0", ).get_property("innerHTML")
     print(song2)
 
     time.sleep(1)
@@ -84,4 +94,4 @@ if __name__ == "__main__":
     print(song10)
 
     browser.quit()
-
+    '''
