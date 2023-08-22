cdcff12150e55a61901e68f1b01acc033eda6b9f
Luca Di Felice <Luca.difelice@hts.on.ca>
2023-08-22 16:54:37 -0400

    Trying to make it print on the artist of each song (it's not working)

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from Today_Top_Hits import Top_50
-
+from Today_Top_Hits import Top_50, Top_50_Artists
 
 if __name__ == "__main__":
     options = Options()
@@ -21,6 +20,8 @@ if __name__ == "__main__":
     for i in range(0, len(Top_50)):
         Song = browser.find_element(By.CSS_SELECTOR, Top_50[i]).get_property("innerHTML")
 
-        print(i + 1, ":", Song)
+        Artist = browser.find_element(By.CSS_SELECTOR, Top_50_Artists[i]).get_property("innerHTML")
+
+        print(i + 1, ":", Song + ",         ", Artist)
 
         time.sleep(0.5)
